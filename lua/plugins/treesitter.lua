51fab539e55a992214fce30fb3eed624c5f7311e
Roconx <arnaurocap@gmail.com>
2023-07-26 11:54:15 +0200

    Add treesitter and mason configs

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "html",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "query",
+        "regex",
+        "typescript",
+        "vim",
+        "yaml",
+        "rust",
+        "bash",
+        "toml",
+        "rust",
+      },
+    },
+  },
+}
