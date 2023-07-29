b63c4812eb2333181b91be53e25ec6fd713580b2
Roconx <arnaurocap@gmail.com>
2023-07-29 20:42:51 +0200

    Add syntax highlighting for nushell scripts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,14 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.nu = {
+  install_info = {
+    url = "https://github.com/nushell/tree-sitter-nu",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "nu",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -21,6 +32,7 @@ return {
         "rust",
         "fish",
         "kdl",
+        "nu", -- Include "nu" if you want to ensure it's installed
       },
     },
   },
