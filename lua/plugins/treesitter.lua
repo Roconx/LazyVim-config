5dba3d71547e04297b9a92cb9330e8b4e30a3883
Roconx <roconx@proton.me>
2023-09-02 10:57:54 +0200

    Remove nu

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,3 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-parser_config.nu = {
-  install_info = {
-    url = "https://github.com/nushell/tree-sitter-nu",
-    files = { "src/parser.c" },
-    branch = "main",
-  },
-  filetype = "nu",
-}
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -32,7 +21,6 @@ return {
         "rust",
         "fish",
         "kdl",
-        "nu", -- Include "nu" if you want to ensure it's installed
       },
     },
   },
