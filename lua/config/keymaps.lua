730dac50e4e7b5f7c8108d4a7bd9c0b96f3ef288
Roconx <roconx@proton.me>
2023-12-24 13:53:47 +0100

    a

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Go to left window", remap = true })
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Go to lower window", remap = true })
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Go to upper window", remap = true })
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Go to right window", remap = true })
