bc1d9eb736057c1d1b296ce40cbda9e232c8fffd
Roconx <roconx@proton.me>
2024-01-28 08:58:38 +0100

    Shift-J and K to move lines up and down

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,5 @@ map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Go to left window", re
 map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Go to lower window", remap = true })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Go to upper window", remap = true })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Go to right window", remap = true })
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down", remap = false, silent = true })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up", remap = false, silent = true })
