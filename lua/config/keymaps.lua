83f41596d75f5fa32d2dd29789da0651a7dc18b7
Roconx <roconx@proton.me>
2024-01-28 17:00:15 +0100

    Disable keymaps and lualine config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,5 +7,31 @@ map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Go to left window", re
 map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Go to lower window", remap = true })
 map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Go to upper window", remap = true })
 map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Go to right window", remap = true })
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down", remap = false, silent = true })
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up", remap = false, silent = true })
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down", remap = false, silent = true })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up", remap = false, silent = true })
+
+-- Disable default buffer related keymaps
+local opts = { noremap = true, silent = true }
+
+map("n", "<S-h>", "<NOP>", opts)
+map("n", "<S-l>", "<NOP>", opts)
+map("n", "[b", "<NOP>", opts)
+map("n", "]b", "<NOP>", opts)
+map("n", "<leader>bb", "<NOP>", opts)
+map("n", "<leader>`", "<NOP>", opts)
+
+-- Disable default move keymaps
+map("n", "<A-j>", "<NOP>", opts)
+map("n", "<A-k>", "<NOP>", opts)
+map("i", "<A-j>", "<NOP>", opts)
+map("i", "<A-k>", "<NOP>", opts)
+map("v", "<A-j>", "<NOP>", opts)
+map("v", "<A-k>", "<NOP>", opts)
+
+-- Disable tab keymaps
+map("n", "<leader><tab>l", "<NOP>", opts)
+map("n", "<leader><tab>f", "<NOP>", opts)
+map("n", "<leader><tab><tab>", "<NOP>", opts)
+map("n", "<leader><tab>]", "<NOP>", opts)
+map("n", "<leader><tab>d", "<NOP>", opts)
+map("n", "<leader><tab>[", "<NOP>", opts)
